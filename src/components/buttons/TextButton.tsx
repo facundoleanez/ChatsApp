f4f1c913b6e9502cea742773e26b360445d23c90
Mate Dev <facundo.leanez@gmail.com>
2023-01-20 15:04:50 +1000

    TextButton Component

---
+++ src/components/buttons/TextButton.tsx
@@ -0,0 +1,30 @@
+import React, {FC} from 'react';
+import {TouchableOpacity} from 'react-native';
+import styled from 'styled-components/native';
+
+//Props types
+interface ComponentProps {
+  main?: boolean;
+}
+interface TextButtonProps {
+  text: string;
+  main?: boolean;
+  handlePress: () => void;
+}
+
+const ButonTitle = styled.Text<ComponentProps>`
+  font-size: 20px;
+  color: ${({theme, main}) =>
+    main ? theme.colors.primary : theme.colors.seccoindaryText};
+  margin: 10px;
+`;
+
+const TextButton: FC<TextButtonProps> = ({main = true, handlePress, text}) => {
+  return (
+    <TouchableOpacity onPress={handlePress}>
+      <ButonTitle main={main}>{text}</ButonTitle>
+    </TouchableOpacity>
+  );
+};
+
+export default TextButton;
--- src/screens/LogIn.tsx
+++ src/screens/LogIn.tsx
@@ -1,8 +1,10 @@
-import React, {useState} from 'react';
-import {TouchableOpacity} from 'react-native';
-import {TextInput} from 'react-native-paper';
+import React, {useState, useRef} from 'react';
+import {TouchableOpacity, TextInput} from 'react-native';
+import {TextInput as TextInputPaper} from 'react-native-paper';
 import styled, {useTheme} from 'styled-components/native';
 import MainButton from '../components/buttons/MainButton';
+import Icons from 'react-native-vector-icons/Feather';
+import TextButton from '../components/buttons/TextButton';
 
 //Views
 const Container = styled.View`
@@ -33,16 +35,14 @@ const SubTitle = styled.Text`
   font-size: 15px;
   color: ${({theme}) => theme.colors.seccoindaryText};
 `;
-const ButtonSecondary = styled.Text`
-  font-size: 20px;
-  color: ${({theme}) => theme.colors.primary};
-  margin: 10px;
-`;
 
 const LogIn = () => {
   const theme = useTheme();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [secureText, setSecureText] = useState(true);
+
+  const inputPasswordRef = useRef<TextInput>(null);
 
   const handlePress = () => {
     console.log(email, password);
@@ -55,10 +55,13 @@ const LogIn = () => {
       <Title>Login</Title>
       <SubTitle>Sign in your account to see your chat</SubTitle>
       <InputContainer>
-        <TextInput
+        <TextInputPaper
           autoComplete="email"
           autoFocus={true}
           blurOnSubmit={false}
+          keyboardType="email-address"
+          maxLength={256}
+          textContentType="emailAddress"
           style={{
             backgroundColor: theme.colors.primaryBackground,
           }}
@@ -68,27 +71,39 @@ const LogIn = () => {
           activeUnderlineColor={theme.colors.primary}
           underlineColor={theme.colors.secondary}
         />
-        <TextInput
+        <TextInputPaper
           autoComplete="off"
           blurOnSubmit={true}
+          keyboardType="default"
+          maxLength={6}
+          secureTextEntry={secureText}
+          textContentType="password"
           style={{backgroundColor: theme.colors.primaryBackground}}
           label="Password"
           value={password}
-          onChangeText={setEmail}
+          onChangeText={setPassword}
           activeUnderlineColor={theme.colors.primary}
           underlineColor={theme.colors.secondary}
-
-          //   onChangeText={text => setText(text)}
+          right={
+            <TouchableOpacity onPress={() => setSecureText(prev => !prev)}>
+              <Icons name="eye" size={10} />
+            </TouchableOpacity>
+          }
+          ref={inputPasswordRef}
         />
       </InputContainer>
-      <ButtonSecondary>Forgget your password?</ButtonSecondary>
-
+      <TextButton
+        text={'Forget your password?'}
+        handlePress={() => console.log('clecked')}
+      />
+      <Icons name="eye" size={10} />
       <MainButton title={'Login'} handlePress={handlePress} />
       <ViewContainer>
         <SubTitle>Do yoy have an account?</SubTitle>
-        <TouchableOpacity>
-          <ButtonSecondary>Sign In</ButtonSecondary>
-        </TouchableOpacity>
+        <TextButton
+          text={'Sign In'}
+          handlePress={() => console.log('clecked')}
+        />
       </ViewContainer>
     </Container>
   );
